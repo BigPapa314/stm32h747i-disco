e6e33261e04de8e0697af48288f080e15ffd7c03
BigPapa314 <Big_Papa@gmx.net>
2020-10-29 08:57:46 +0100

    Added blinky example

--- examples/hello.rs
+++ examples/hello.rs
@@ -1,20 +1,25 @@
 #![no_main]
 #![no_std]
 
-use stm32h747i_disco as board;
+use bsp::Board;
+use stm32h747i_disco as bsp;
 
 use defmt_rtt as _; // global logger
 use panic_probe as _;
 
 #[cortex_m_rt::entry]
 fn main() -> ! {
+    let board = Board::constrain().expect("could not get the board!");
+
     defmt::info!("Hello, world1!");
     defmt::info!("Hello, world2!");
 
-    board::exit();
+    board.release();
+
+    bsp::exit();
 }
 
 #[defmt::timestamp]
 fn timestamp() -> u64 {
-    board::timestamp()
+    bsp::timestamp()
 }
